f213bfa9a05110b5bd6f38d22eab01c1b7b2b27e
Alex <sanchos124@gmail.com>
2023-12-18 20:09:29 +0200

    Row Methods completed

--- main.js
+++ main.js
@@ -1,42 +1,61 @@
-let confirmResult = confirm("Tell me three most important words 💚");
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-if (confirmResult) {
-  let words = [];
-  let i = 0;
+let userInput = prompt("Введіть довільний рядок:");
+let userChoice = parseInt(prompt("Введіть ID операції (ціле число):"));
 
-  while (i < 3) {
-    let word = prompt(`Enter word ${i + 1}:`);
-    if (word === null || word === "" || /\d/.test(word)) {
-      continue;
-    }
-
-    let format = prompt(
-      `Choose the format for '${word}': (uppercase, lowercase, capitalize)`
+switch (userChoice) {
+  case 1:
+    let index = parseInt(
+      prompt("Введіть індекс для отримання Unicode значення символу:")
     );
-    if (
-      format === "uppercase" ||
-      format === "lowercase" ||
-      format === "capitalize"
-    ) {
-      switch (format) {
-        case "uppercase":
-          word = word.toUpperCase();
-          break;
-        case "lowercase":
-          word = word.toLowerCase();
-          break;
-        case "capitalize":
-          word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-          break;
-        default:
-          break;
-      }
-      words.push(word);
-      i++;
-    }
-  }
+    let unicodeValue = userInput.charCodeAt(index);
+    console.log(
+      `Unicode значення символу на позиції ${index}: ${unicodeValue}`
+    );
+    break;
 
-  console.log(`${words[0]} ${words[1]} ${words[2]}!`);
-} else {
-  console.log("Operation canceled.");
+  case 2:
+    let removeIndex = parseInt(prompt("Введіть індекс для видалення символу:"));
+    let stringWithoutChar =
+      userInput.slice(0, removeIndex) + userInput.slice(removeIndex + 1);
+    console.log(
+      `Рядок після видалення символу на позиції ${removeIndex}: ${stringWithoutChar}`
+    );
+    break;
+
+  case 3:
+    let replaceIndex = parseInt(prompt("Введіть індекс для заміни символу:"));
+    let randomSmileys = [
+      "😀",
+      "😊",
+      "🤖",
+      "🌟",
+      "🎈",
+      "🍀",
+      "🎉",
+      "🚀",
+      "💡",
+      "🎶",
+    ];
+    let randomIndex = getRandomIntInclusive(0, 9);
+    let replacedString =
+      userInput.substring(0, replaceIndex) +
+      randomSmileys[randomIndex] +
+      userInput.substring(replaceIndex + 1);
+    console.log(
+      `Рядок після заміни символу на позиції ${replaceIndex}: ${replacedString}`
+    );
+    break;
+
+  case 4:
+    let letterCount = userInput.replace(/\s/g, "").length;
+    console.log(`Кількість літер у рядку (без пробілів): ${letterCount}`);
+    break;
+
+  default:
+    console.log("Невірний ID операції.");
 }
